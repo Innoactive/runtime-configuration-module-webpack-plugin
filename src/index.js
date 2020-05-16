34b7a7f5494c1f7c674da69b93a9f9592a488486
Benedikt Reiser <benedikt.reiser@gmail.com>
2020-05-16 22:02:52 +0200

    fix: return placeholder for set but empty env var

--- src/index.js
+++ src/index.js
@@ -7,20 +7,36 @@ import validateOptions from 'schema-utils';
 import schema from './options.json';
 
 /**
+ * Given a parameter's name, returns either the value of an environment variable
+ * with the same name or a string placeholder with the parameter's name
+ * @param {string} parameterName the name of the parameter
+ */
+export const getDefaultValueForParameter = (parameterName) =>
+  parameterName in process.env
+    ? process.env[parameterName]
+    : `\${${parameterName}}`;
+
+/**
  * Iterates over the given Array of Configuration Parameter (names) and returns a dictionary
  * containing a key for each configuration parameter with the actual value of an environment
  * variable (if set) or a placeholder value of the form "${VARNAME}" alternatively
  * @param {Array} configurationParameters
  */
-const getValuesForConfigurationParameters = (configurationParameters = []) => {
-  const values = {};
-  configurationParameters.forEach((parameter) => {
-    values[parameter] = process.env[parameter] || `\${${parameter}}`;
-  });
-  return values;
+const getDefaultValuesForConfigurationParameters = (
+  configurationParameters = []
+) => {
+  return Object.fromEntries(
+    new Map(
+      // map each configuration parameter to an array with:
+      // first element being the parameter itself
+      // second element being the value of an environment variable with the
+      // parameter's name or a placeholder with the parameter's name
+      configurationParameters.map((parameter) => [parameter, ,])
+    )
+  );
 };
 
-class RuntimeConfigurationModulePlugin {
+export default class RuntimeConfigurationModulePlugin {
   constructor(options) {
     validateOptions(schema, options, {
       name: 'Runtime Configuration Module Plugin',
@@ -35,7 +51,7 @@ class RuntimeConfigurationModulePlugin {
     // convert parameterNames into an object looking like
     // eslint-disable-next-line no-irregular-whitespace
     // { KEY: process.env.KEY || "$KEY" }
-    const config = getValuesForConfigurationParameters(parameters);
+    const config = getDefaultValuesForConfigurationParameters(parameters);
 
     // read in the major parts of the runtime configuration module
     // (helper functions that ensure no variable placeholders remain once
@@ -59,5 +75,3 @@ class RuntimeConfigurationModulePlugin {
     }).apply(compiler);
   }
 }
-
-module.exports = RuntimeConfigurationModulePlugin;
---
+++ test/index.test.js
@@ -0,0 +1,61 @@
+import { getDefaultValueForParameter } from '../src';
+
+describe('getDefaultValueForParameter', () => {
+  const ORIGINAL_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules();
+    // unset all environment variables for easier testing
+    process.env = {};
+  });
+
+  afterEach(() => {
+    // restore all environmnt variables
+    process.env = ORIGINAL_ENV;
+  });
+
+  test('returns value of environment variable if set', () => {
+    // Given an environment variable
+    process.env.MY_PARAMETER = 'test';
+
+    // When trying to get the default parameter for runtime configuration
+    const defaultValue = getDefaultValueForParameter('MY_PARAMETER');
+
+    // Then expect the value to be the value of the environment variable
+    expect(defaultValue).toBe('test');
+  });
+
+  test("returns a placeholder w/ the parameter's name if environment variable not set", () => {
+    // Given nothing
+    // When trying to get the default parameter for runtime configuration
+    const defaultValue = getDefaultValueForParameter('MY_PARAMETER');
+
+    // Then expect the value to be the placeolder for this parameter
+    // eslint-disable-next-line no-template-curly-in-string
+    expect(defaultValue).toBe('${MY_PARAMETER}');
+  });
+
+  test('returns empty value if environment variable set to empty', () => {
+    // Given an environment variable that is set to an empty string
+    process.env.MY_PARAMETER = '';
+
+    // When trying to get the default parameter for runtime configuration
+    const defaultValue = getDefaultValueForParameter('MY_PARAMETER');
+
+    // Then expect the value to be the value of the environment variable
+    expect(defaultValue).toBe('');
+  });
+
+  test('returns undefined value if environment variable set to undefined', () => {
+    // Given an environment variable that is set to undefined
+    // eslint-disable-next-line no-undefined
+    process.env.MY_PARAMETER = undefined;
+
+    // When trying to get the default parameter for runtime configuration
+    const defaultValue = getDefaultValueForParameter('MY_PARAMETER');
+
+    // Then expect the value to be the value of the environment variable
+    // eslint-disable-next-line no-undefined
+    expect(defaultValue).toBe(undefined);
+  });
+});
